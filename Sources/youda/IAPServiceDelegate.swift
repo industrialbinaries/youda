2617cb7425820492c1e13293c02061938e61b280
Jan Timar <jano@industrial-binaries.co>
2020-01-23 11:07:33 +0100

    Add IAPServiceDelegate

---
+++ Sources/youda/IAPServiceDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  youda
+//
+//  Copyright (c) 2020 Industrial Binaries
+//  MIT license, see LICENSE file for details
+//
+
+import Foundation
+
+/// IAP Service delegate for IAP updates
+public protocol IAPServiceDelegate: class {
+  /// Call when `IAPService` update `purchasedProducts`, this function call as many times as add new product
+  /// - Parameter purchasedProducts: Set of current `purchasedProducts`
+  func didUpdate(purchasedProducts: Set<InAppProductId>)
+}
